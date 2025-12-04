3e31732d54ddbb9e3c2819ee744aa53715dac1f9
dufeuremi <dufeuremi@gmail.com>
2025-12-04 19:05:01 +0100

    Ajout du code backend complet

---
+++ pb_migrations/1764366776_updated_news.js
@@ -0,0 +1,29 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_987692768")
+
+  // add field
+  collection.fields.addAt(7, new Field({
+    "hidden": false,
+    "id": "file3112513328",
+    "maxSelect": 1,
+    "maxSize": 0,
+    "mimeTypes": [],
+    "name": "preview",
+    "presentable": false,
+    "protected": false,
+    "required": false,
+    "system": false,
+    "thumbs": [],
+    "type": "file"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_987692768")
+
+  // remove field
+  collection.fields.removeById("file3112513328")
+
+  return app.save(collection)
+})
